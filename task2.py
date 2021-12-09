6f9dafb2ef1abc52622704ab18accf39d6166a94
elinorav <elinorav@post.bgu.ac.il>
2021-12-09 16:16:14 +0200

    start scraping pages

--- task2.py
+++ task2.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     df = pd.DataFrame(data, columns=['url'])
     for d in df.values:
         html_page, e = reco.scan_landing_page("https://www.oceandraw.com/worldwide/former-wrestlers-ta?utm_campaign=t-od-wrestlers-s-d-ww-091220&utm_medium=taboola&utm_source=taboola&utm_term=disqus-widget-safetylevel20longtail09")
+        # html_page, e = reco.scan_landing_page(d[0])
         if html_page is not None:
             f = open('test.txt', 'w+')
             tags = html_page.find_all()
