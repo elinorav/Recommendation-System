9064e6ef5e4813ab1aa9d54eb6eed7fac64b4adc
elinorav <elinorav@post.bgu.ac.il>
2021-12-15 13:57:50 +0200

    add description

--- task_scrap_pages/task1.py
+++ task_scrap_pages/task1.py
@@ -70,12 +70,14 @@ def print_hi():
 
 
 if __name__ == '__main__':
-    i = 0
-    data = pd.read_csv(r'ds_anstrex_tb.csv')
-
-    data["keywords"] = ""
-
-    df = pd.DataFrame(data, columns=['url'])
+    url = "https://www.sonoviastore.co.il/blogs/news/%D7%94%D7%9E%D7%A1%D7%9B%D7%94-%D7%94%D7%98%D7%95%D7%91%D7%94-%D7%91%D7%99%D7%95%D7%AA%D7%A8-%D7%9C%D7%98%D7%99%D7%A1%D7%95%D7%AA-%D7%91%D7%A9%D7%A0%D7%AA-2021-%D7%A1%D7%95%D7%A0%D7%95%D7%91%D7%99%D7%94?utm_source=taboola&utm_medium=referral&utm_campaign=TB_Blog_AirTravel_IL_PC&tblci=GiDbJRndUImP9rc80Mls7KW1gFpDdEMCGlkTelmGFUrFzyDq3U8oxaqc6ZT4jY_QAQ#tblciGiDbJRndUImP9rc80Mls7KW1gFpDdEMCGlkTelmGFUrFzyDq3U8oxaqc6ZT4jY_QAQ"
+    print(scrap(url))
+    # i = 0
+    # data = pd.read_csv(r'ds_anstrex_tb.csv')
+    #
+    # data["keywords"] = ""
+    #
+    # df = pd.DataFrame(data, columns=['url'])
 
     # count urls
     # count = df.count()[0]
@@ -88,13 +90,13 @@ if __name__ == '__main__':
 
     # executor.submit(thread_extract_data_from_url)
 
-
-    for d in df.values:
-       try:
-           list = extract_keywords(d[0])
-           data['keywords'].iloc[i] = list
-           data.to_csv("ds_anstrex_tb.csv", index=False)
-       except Exception as e:
-           print("index: " + str(i) + ": " + str(e))
-       print(str(i))
-       i = i + 1
+    #
+    # for d in df.values:
+    #    try:
+    #        list = extract_keywords(d[0])
+    #        data['keywords'].iloc[i] = list
+    #        data.to_csv("ds_anstrex_tb.csv", index=False)
+    #    except Exception as e:
+    #        print("index: " + str(i) + ": " + str(e))
+    #    print(str(i))
+    #    i = i + 1
--- task_scrap_pages/task2.py
+++ task_scrap_pages/task2.py
@@ -6,13 +6,15 @@ if __name__ == '__main__':
     reco = recoSystem.RecoSystem()
     data = pd.read_csv(r'ds_anstrex_tb.csv')
     df = pd.DataFrame(data, columns=['url'])
+    index = 0
     for d in df.values:
-        html_page, e = reco.scan_landing_page("https://www.oceandraw.com/worldwide/former-wrestlers-ta?utm_campaign=t-od-wrestlers-s-d-ww-091220&utm_medium=taboola&utm_source=taboola&utm_term=disqus-widget-safetylevel20longtail09")
-        # html_page, e = reco.scan_landing_page(d[0])
+        html_page, e = reco.scan_landing_page(d[0])
         if html_page is not None:
-            f = open('test.txt', 'w+')
+            f = open("page_"+str(index), 'w', encoding="utf-8")
             tags = html_page.find_all()
             str_res = ""
             for t in tags:
                 str_res += t.text
             f.write(str_res)
+            f.close()
+        index += 1
